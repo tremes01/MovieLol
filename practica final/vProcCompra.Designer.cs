be69d488894a257b26ab0c76f61aceab62d33f2d
unknown <jean000200@gmail.com>
2019-10-27 12:42:58 -0400

    .

--- practica final/vProcCompra.Designer.cs
+++ practica final/vProcCompra.Designer.cs
@@ -28,11 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtprecio = new System.Windows.Forms.Label();
+            this.txtDisponible = new System.Windows.Forms.Label();
+            this.txtSala = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 233);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Comprar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtprecio
+            // 
+            this.txtprecio.AutoSize = true;
+            this.txtprecio.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtprecio.Location = new System.Drawing.Point(260, 106);
+            this.txtprecio.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtprecio.Name = "txtprecio";
+            this.txtprecio.Size = new System.Drawing.Size(44, 13);
+            this.txtprecio.TabIndex = 4;
+            this.txtprecio.Text = "Precio: ";
+            this.txtprecio.Click += new System.EventHandler(this.txtprecio_Click);
+            // 
+            // txtDisponible
+            // 
+            this.txtDisponible.AutoSize = true;
+            this.txtDisponible.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDisponible.Location = new System.Drawing.Point(359, 30);
+            this.txtDisponible.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtDisponible.Name = "txtDisponible";
+            this.txtDisponible.Size = new System.Drawing.Size(71, 13);
+            this.txtDisponible.TabIndex = 5;
+            this.txtDisponible.Text = "Disponibles:";
+            // 
+            // txtSala
+            // 
+            this.txtSala.AutoSize = true;
+            this.txtSala.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.txtSala.Location = new System.Drawing.Point(258, 20);
+            this.txtSala.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txtSala.Name = "txtSala";
+            this.txtSala.Size = new System.Drawing.Size(51, 25);
+            this.txtSala.TabIndex = 6;
+            this.txtSala.Text = "Sala:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.label1.Location = new System.Drawing.Point(105, -59);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 37);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nombre de la pelicula";
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Location = new System.Drawing.Point(49, 20);
+            this.pictureBox9.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(185, 226);
+            this.pictureBox9.TabIndex = 9;
+            this.pictureBox9.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(325, 74);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(31, 20);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.Text = " ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(260, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = " Cantidad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(260, 133);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = " Descuento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(260, 165);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = " Total:";
+            this.label4.Click += new System.EventHandler(this.txtprecio_Click);
+            // 
+            // vProcCompra
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(490, 286);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtprecio);
+            this.Controls.Add(this.txtDisponible);
+            this.Controls.Add(this.txtSala);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox9);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "vProcCompra";
             this.Text = "vProcCompra";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label txtprecio;
+        private System.Windows.Forms.Label txtDisponible;
+        private System.Windows.Forms.Label txtSala;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- practica final/vProcCompra.cs
+++ practica final/vProcCompra.cs
@@ -16,5 +16,10 @@ namespace practica_final
         {
             InitializeComponent();
         }
+
+        private void txtprecio_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
